a4a1e5cdd278b929945d66492e6697e94d048673
salomeriverod-hue <salomeriverod@gmail.com>
2026-03-29 21:25:06 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -10,7 +10,7 @@ st.title("TF-IDF con Preguntas y Respuestas")
 
 # Ejemplo inicial en inglés
 text_input = st.text_area(
-  "Escribe tus documentos (uno por línea, en inglés):",
+  "Lee las siguientes oraciones:",
     "Isabella is at class.\nManuela is eating her lunch.\nSamuel is with his grandpa."
 )
 
